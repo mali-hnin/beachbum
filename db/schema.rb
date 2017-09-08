00f2240f2fc287acd37c042c3fc864fc55b67845
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-09-08 19:07:06 +0100

    added seeds

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170908154156) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "beaches", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "category"
+    t.boolean "bar"
+    t.integer "price"
+    t.boolean "nudist"
+    t.boolean "nude_friendly"
+    t.boolean "pet_friendly"
+    t.boolean "surf"
+    t.string "beach_sports", array: true
+    t.string "water_sports", array: true
+    t.integer "crowd"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "country"
+    t.string "city"
+    t.boolean "parking"
+    t.boolean "public_transportation"
+    t.integer "walking_distance"
+    t.string "sand_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
