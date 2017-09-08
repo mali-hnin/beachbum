ef434c364e6c3644ba6adee3b9dfdcd0226c3818
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-09-08 21:24:42 +0100

    icons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,11 @@
 class PagesController < ApplicationController
-  def index
-    @beaches = Beach.where.not(latitude: nil, longitude: nil)
-
-    @hash = Gmaps4rails.build_markers(@beaches) do |beach, marker|
-      marker.lat .latitude
-      marker.lng flat.longitude
-    end
-  end
-
   def home
     @beaches = Beach.where.not(latitude: nil, longitude: nil)
 
     @hash = Gmaps4rails.build_markers(@beaches) do |beach, marker|
-      marker.lat flat.latitude
-      marker.lng flat.longitude
-      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+      marker.lat beach.latitude
+      marker.lng beach.longitude
+      # marker.infowindow render_to_string(partial: "/pages/map_box", locals: { beach: beach })
     end
   end
 end
