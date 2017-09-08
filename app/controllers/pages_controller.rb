305874983e70b5b87ffa5c5e121f589e3909f5ba
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-09-08 18:46:36 +0100

    gmaps scripts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @beaches = Beach.where.not(latitude: nil, longitude: nil)
+
+    @hash = Gmaps4rails.build_markers(@beaches) do |beach, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+    end
   end
 end
