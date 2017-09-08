77d7b63f32eea562b3762dbd94cfb02b1054cf7e
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-09-08 16:42:19 +0100

    added css structure and beach model

---
+++ db/migrate/20170908154156_create_beaches.rb
@@ -0,0 +1,28 @@
+class CreateBeaches < ActiveRecord::Migration[5.1]
+  def change
+    create_table :beaches do |t|
+      t.string :name
+      t.string :location
+      t.string :category
+      t.boolean :bar
+      t.integer :price
+      t.boolean :nudist
+      t.boolean :nude_friendly
+      t.boolean :pet_friendly
+      t.boolean :surf
+      t.array :beach_sports
+      t.array :water_sports
+      t.integer :crowd
+      t.float :latitude
+      t.float :longitude
+      t.string :country
+      t.string :city
+      t.boolean :parking
+      t.boolean :public_transportation
+      t.integer :walking_distance
+      t.string :sand_type
+
+      t.timestamps
+    end
+  end
+end
